05feef7bf5b975c53b1ee15f37edf49c8612a675
TouMingQAQ <lxlmeng@qq.com>
2022-10-04 09:32:50 +0800

    测试item

---
+++ MVC/Model/Item/BackpackItems/TestItem.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "TestItem",menuName = "Item/TestItem")]
+public class TestItem : BaseItemScript,IItem
+{
+    
+    public void Execute()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void SetDestroy()
+    {
+        throw new System.NotImplementedException();
+    }
+}
